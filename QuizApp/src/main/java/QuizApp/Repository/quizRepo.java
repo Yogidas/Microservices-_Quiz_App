8f980435671f580f25003e4de529f648f1e608aa
yogidas <yogeshvaishnav332@gmail.com>
2026-06-08 16:43:16 +0530

    Remove Monolithic Quiz

--- QuizApp/src/main/java/QuizApp/Repository/quizRepo.java
+++
@@ -1,9 +0,0 @@
-package QuizApp.Repository;
-
-import QuizApp.Entity.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface quizRepo extends JpaRepository<Quiz,Integer> {
-}
